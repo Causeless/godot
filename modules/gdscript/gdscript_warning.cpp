49851a435a63e8cb2bcb7fa488a63c424ab2bf40
Causeless <tommymarch5@gmail.com>
2025-09-17 18:54:54 +0100

    Warn on unprefixed class members

--- modules/gdscript/gdscript_warning.cpp
+++ modules/gdscript/gdscript_warning.cpp
@@ -164,6 +164,9 @@ String GDScriptWarning::get_message() const {
 			return vformat(R"*(The default value uses "%s" which won't return nodes in the scene tree before "_ready()" is called. Use the "@onready" annotation to solve this.)*", symbols[0]);
 		case ONREADY_WITH_EXPORT:
 			return R"("@onready" will set the default value after "@export" takes effect and will override it.)";
+		case UNPREFIXED_MEMBER:
+			CHECK_SYMBOLS(1);
+			return vformat(R"(The class member variable "%s" is not prefixed, such as with a leading "m_" or "e_".)", symbols[0]);
 #ifndef DISABLE_DEPRECATED
 		// Never produced. These warnings migrated from 3.x by mistake.
 		case PROPERTY_USED_AS_FUNCTION: // There is already an error.
@@ -241,6 +244,7 @@ String GDScriptWarning::get_name_from_code(Code p_code) {
 		PNAME("NATIVE_METHOD_OVERRIDE"),
 		PNAME("GET_NODE_DEFAULT_WITHOUT_ONREADY"),
 		PNAME("ONREADY_WITH_EXPORT"),
+		PNAME("UNPREFIXED_MEMBER"),
 #ifndef DISABLE_DEPRECATED
 		"PROPERTY_USED_AS_FUNCTION",
 		"CONSTANT_USED_AS_FUNCTION",
